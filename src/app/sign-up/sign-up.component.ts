31947d3db3427b6ae42a63eb29d13e511455628f
FavourNedabia <senaadjoe@gmail.com>
2025-02-13 14:48:03 +0000

    singup page

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../auth-service/auth.service'; 
-
+import { AuthService } from '../auth-service/auth.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,26 +10,21 @@ export class SignUpComponent {
   username: string = '';
   password: string = '';
   confirmPassword: string = '';
+  passwordMismatch: boolean = false;
 
   constructor(private authService: AuthService) {}
 
   onSubmit() {
     if (this.password !== this.confirmPassword) {
-      alert("Les mots de passe ne correspondent pas.");
+      this.passwordMismatch = true;
       return;
     }
 
-    this.authService.signUp(this.username, this.password, this.confirmPassword)
-      .subscribe(
-        response => {
-          alert("Inscription réussie !");
-        },
-        error => {
-          console.error("Erreur lors de l'inscription", error);
-          alert("Erreur lors de l'inscription");
-        }
-      );
+    this.passwordMismatch = false;
+
+    this.authService.signUp(this.username, this.password, this.confirmPassword).subscribe({
+      next: (response) => console.log("Inscription réussie", response),
+      error: (error) => console.error("Erreur d'inscription", error)
+    });
   }
 }
-
-
